782960b9751bb91f629fd46fb19286186b27b4e5
Algortm <116910256+Algortm@users.noreply.github.com>
2024-08-11 10:31:03 +0300

    tests

--- src/main/java/org/example/test.java
+++ src/main/java/org/example/test.java
@@ -23,7 +23,7 @@ public class test {
         System.out.println(Arrays.toString(arrTarget));
         int number =5;
         System.out.println(number % 2 == 0 ? "even" : "odd");*/
-        int num = 8;
+        /*int num = 8;
         String digitName;
 
         switch(num) {
@@ -60,6 +60,43 @@ public class test {
             default:
                 digitName = "Unknown";
         }
-        System.out.println(digitName);
+        System.out.println(digitName);*/
+       /* Scanner scanner = new Scanner(System.in);
+
+        boolean a = scanner.nextBoolean();
+        boolean b = scanner.nextBoolean();
+        boolean c = scanner.nextBoolean();
+        boolean i = a&&b&&c;
+        boolean o = a||b||c;
+
+        System.out.println(i + " " + o);*/
+        //char[][] mas = new char[4][3];
+       /* char[][] mas= {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'},
+                {'*', '0', '#'}
+        };
+        System.out.print(Arrays.toString(mas[0])+"\n");`
+        System.out.print(Arrays.toString(mas[1])+"\n");
+        System.out.print(Arrays.toString(mas[2])+"\n");
+        System.out.print(Arrays.toString(mas[3]));
+        System.out.print(Arrays.toString(mas[0])+"\n"+Arrays.toString(mas[1])+"\n"+Arrays.toString(mas[2])+"\n"+ Arrays.toString(mas[3]));
+*/
+        test obj = new test();
+        //System.out.println(obj.aggregateSingle("Mark", "22", "Earth"));
+        String[] masName = {"mark","oleg","Jhuk"};
+        int[] masAge = {22,21,20};
+        String[] masPlanet= {"Mars","Earth","Sun"};
+        System.out.println(Arrays.toString(obj.aggregateAll(masName,masAge, masPlanet)));
+    }
+    public String aggregateSingle(String name, int age, String planet){
+
+        return "name -  <" + name.toUpperCase() + ">, age - <" + age+ ">, planet - <"+planet.toUpperCase() + ">";
+    }
+    public String[] aggregateAll(String[] names, int[] ages, String[] planets){
+        String[] mas = {aggregateSingle(names[0], ages[0], planets[0]), aggregateSingle(names[1], ages[1], planets[1]), aggregateSingle(names[2], ages[2], planets[2])};
+        return mas;
+    
     }
 }
